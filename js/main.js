4e32c2bad8c829ed33494af7f944770eb9ab9d09
ICee-6717OK <okumuisidore@gmail.com>
2020-07-19 03:36:00 +0300

    modify js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
 
 function giveAkanName () {
-    var year =parseInt(document.getElementById("year").value);
-    var month=parseInt(document.getElementById("month").value);
-    var day=parseInt(document.getElementById("day").value);
-    var gender=getGender();
-    var date = new Date(year +"/" + month + "/"+day);
-    var dayBorn = date.getDay();
-    // var akanName;
-    var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
-    // var validateGender = (gender !== "male" && gender !== "female");
+  var year =parseInt(document.getElementById("year").value);
+  var month=parseInt(document.getElementById("month").value);
+  var day=parseInt(document.getElementById("day").value);
+  var gender=getGender();
+  var date = new Date(year +"/" + month + "/"+day);
+  var dayBorn = date.getDay();
+  // var akanName;
+  var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
+  // var validateGender = (gender !== "male" && gender !== "female");
 
     if (year <= 1800 ) {
       alert("Invalid year");
